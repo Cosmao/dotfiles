1e9c802378bdbf3ec37f2a46fe2fb4c20740a02a
Emil K <29043737+Cosmao@users.noreply.github.com>
2024-12-10 12:39:44 +0100

    Working absolute wallpaper setter

--- .config/scripts/wallpaper.sh
+++ .config/scripts/wallpaper.sh
@@ -4,12 +4,14 @@
 #
 
 # Variables
-DIR=$HOME/Wallpapers # TODO: Change to export
+DIR=$WALLPAPERPATH
 USERANDOM="false"
 PREVIEW="false"
 FILE=""
 VERBOSE="false"
+ABSOLUTE="false"
 FUZZY="false"
+SHOWCOLOUR="true"
 
 
 getCurrentWallpaper(){
@@ -22,41 +24,81 @@ getCurrentWallpaper(){
 }
 
 setWallpaper(){
+  if [ "$VERBOSE" == "true" ]; then
+    echo "Checking if already in use."
+  fi
   getCurrentWallpaper $1
   ALREADYUSED=$?
   if [[ $ALREADYUSED -eq 1 ]]; then
-    echo "Already using that wallpaper"
+    echo "Already using that wallpaper."
     exit
   fi
 
+  if [ "$VERBOSE" == "true" ]; then
+    echo "Preloading wallpaper."
+  fi
   PRELOAD=$(hyprctl hyprpaper preload $1)
   if [ ! "$PRELOAD" == "ok" ]; then
-    echo "Failed to preload"
-    echo $PRELOAD
+    echo "Failed to preload."
+    if [ "$VERBOSE" == "true" ]; then
+      echo $PRELOAD
+    fi
     exit 1
   fi
 
   # Set the wallpaper and run pywal
+  if [ "$VERBOSE" == "true" ]; then
+    echo "Setting wallpaper."
+  fi
   SETPAPER=$(hyprctl hyprpaper wallpaper ",$1")
+
+  if [ "$VERBOSE" == "true" ]; then
+    echo "Generating colours."
+  fi
   PYWAL=$(wal -i $1 -n -t -e)
+
+  if [ "$VERBOSE" == "true" ]; then
+    echo "Unloading old wallpaper."
+  fi
   UNLOAD=$(hyprctl hyprpaper unload unused)
-  echo $(wal -i $1 --preview)
+
+  if [ "$SHOWCOLOUR" == "true" ]; then
+    echo $(wal -i $1 --preview)
+  fi
 
   # Update hyprpaper file
+  #
+  if [ "$VERBOSE" == "true" ]; then
+    echo "Writing to hyprpaper.conf"
+  fi
   sed -i "s|^preload.*|preload = ${1}|" "$(realpath ~/.config/hypr/hyprpaper.conf)"
   sed -i "s|^wallpaper.*|wallpaper = ,${1}|" "$(realpath ~/.config/hypr/hyprpaper.conf)"
 
   # Get colours to waybar
+  if [ "$VERBOSE" == "true" ]; then
+    echo "Setting waybar colours."
+  fi
   cp ~/.cache/wal/colors-waybar.css ~/dotfiles/.config/waybar/ >/dev/null 2>&1
 
   # terminate running instances
+  if [ "$VERBOSE" == "true" ]; then
+    echo "Killing waybar."
+  fi
   killall -q waybar >/dev/null 2>&1
 
   # wait until processes have been shut down
+
   while pgrep -x waybar >/dev/null; do sleep 0.1; done
 
   # Relaunch waybar
+  if [ "$VERBOSE" == "true" ]; then
+    echo "Starting waybar."
+  fi
   hyprctl dispatch exec waybar >/dev/null 2>&1
+
+  if [ "$VERBOSE" == "true" ]; then
+    echo "Done."
+  fi
 }
 
 usage(){
@@ -65,40 +107,60 @@ usage(){
    echo
    echo "Syntax: scriptTemplate [-r|p|f|F|v|d|h]"
    echo "options:"
-   echo "r     Selects a random wallpaper in your wallpaper directory."
+   echo "r     Selects a random wallpaper in your wallpaper directory. Mutually exclusive with -f and -F."
    echo "p     Shows a preview of your selected wallpaper before you change."
-   echo "f     Filename of the file you wish to use as a wallpaper."
-   echo "F     Fuzzy find a file inside the wallpaper directory."
+   echo "f     Filename of the file you wish to use as a wallpaper. Mutually exclusive with -r and -F."
+   echo "F     Fuzzy find a file inside the wallpaper directory. Mutually exclusive with -f and -r."
    echo "v     Verbose mode."
    echo "d     Directory you wish to get your wallpaper from if not set as an export."
+   echo "s     Suppress the colour preview."
    echo "h     Print this Help."
    echo
 }
 
-while getopts ":r :p :f: :F: :v :d: :h" flag; do
+# Handle all arguments
+while getopts "rpvsf:F:d:h" flag ; do
   case "${flag}" in
     r) USERANDOM="true" ;;
     p) PREVIEW="true" ;;
-    f) FILE=${OPTARG} ;;
-    F) FILE=${OPTARG} FUZZY="true" ;;
     v) VERBOSE="true" ;;
+    s) SHOWCOLOUR="false" ;;
+    f) FILE=${OPTARG}; ABSOLUTE="true" ;;
+    F) FILE=${OPTARG}; FUZZY="true" ;;
     d) DIR=${OPTARG} ;;
-    h) usage exit 0 ;;
-    ?) echo "Error: Invalid option was specified -$OPTARG"; exit 1 ;;
+    h) usage; exit 0 ;;
+    ?) echo "Error: Invalid option -$OPTARG"; exit 1 ;;
   esac
 done
 
+# Make sure directory exists
 if [ ! -d $DIR ]; then
   echo "Wallpaper directory not found"
   exit 1
 fi
 
-if [ ! -z $FILE ] && [ "$USERANDOM" == "true" ]; then
-  usage
-  if [ "$VERBOSE" == "true" ]; then
-    echo "Cant send random and a specific file!"
-  fi
+WALLPAPEROPTS=0
+[[ "$ABSOLUTE" == "true" ]] && ((WALLPAPEROPTS++))
+[[ "$FUZZY" == "true" ]] && ((WALLPAPEROPTS++))
+[[ "$USERANDOM" == "true" ]] && ((WALLPAPEROPTS++))
+
+# Sanity checking args
+if ((WALLPAPEROPTS > 1)); then
+  echo "Error: -r, -f, and -F are mutually exclusive. Only one can be true."
   exit 1
+elif ((WALLPAPEROPTS == 0)); then
+  echo "Error: -r, -f or -F flag is needed."
+  exit 1
+fi
+
+if [ "$ABSOLUTE" == "true" ]; then
+  if [ -f $DIR/$FILE ]; then
+    setWallpaper $DIR/$FILE
+    exit 0
+  else
+    echo "File $DIR/$FILE doesn't exist."
+    exit 1
+  fi
 fi
 
 # We send a picture to use
