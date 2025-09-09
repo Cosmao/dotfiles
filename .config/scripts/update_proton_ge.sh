c7c92d6c8dd66695d99fe8ba56b9af4ddd85f241
Emil K <29043737+Cosmao@users.noreply.github.com>
2025-09-09 15:45:44 +0200

    mid update

--- .config/scripts/update_proton_ge.sh
+++ .config/scripts/update_proton_ge.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -euo pipefail
-CURL_RESPONSE=""
-LATEST_VERSION=""
-TARBALL_URL=""
-TARBALL_NAME=""
-SHA512_URL=""
-SHA512_NAME=""
-DIR_NAME=""
-LATEST_URL="https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest"
 
-STEAM_PATH="$(realpath "$HOME/.steam/steam/compatibilitytools.d")"
+declare -A TARBALLS
+declare -A SHA512
+declare -A INSTALLED
+
+RELEASES_URL="https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases"
+#STEAM_PATH="$(realpath "$HOME/.steam/steam/compatibilitytools.d")"
 
 for tool in jq rg sha512sum curl tar basename realpath; do
     command -v $tool >/dev/null 2>&1 || { echo >&2 "$tool is required but not installed."; exit 1; }
@@ -21,38 +18,53 @@ if [[ ! -d "$WORKDIR" || "$WORKDIR" != /tmp/* ]]; then
     echo "Failed to create a safe temporary directory."
     exit 1
 fi
+cd "$WORKDIR"
 
 trap '[[ -d "$WORKDIR" && "$WORKDIR" == /tmp/* ]] && rm -rf -- "$WORKDIR"' EXIT
-cd "$WORKDIR"
 
 function get_urls(){
    echo "Getting versions"
    local CURL_URL="$1"
-   CURL_RESPONSE=$(curl -s $CURL_URL)
-   LATEST_VERSION=$(echo "$CURL_RESPONSE" | jq -r ."tag_name")
-   ASSETS_LENGTH=$(echo "$CURL_RESPONSE" | jq -r ."assets | length")
+   local CURL_RESPONSE=$(curl -s $CURL_URL)
+   local NUM_RELEASES=$(echo "$CURL_RESPONSE" | jq -r "length")
+   echo $NUM_RELEASES
 
-   for ((i = 0 ; i < $ASSETS_LENGTH ; i++)); do
-      ASSET_NAME=$(echo "$CURL_RESPONSE" | jq -r ."assets[$i].name")
-      if echo "$ASSET_NAME" | rg -q ".tar.gz" ; then
-         TARBALL_URL=$(echo "$CURL_RESPONSE" | jq -r ."assets[$i].browser_download_url")
-         echo "Found tarball"
-      fi
-      if echo "$ASSET_NAME" | rg -q ".sha512sum" ; then
-         SHA512_URL=$(echo "$CURL_RESPONSE" | jq -r ."assets[$i].browser_download_url")
-         echo "Found SHA512"
+   for ((num = 0; num < $NUM_RELEASES ; num++)); do
+      local TARBALL_URL=""
+      local SHA512_URL=""
+      local VERSION=$(echo "$CURL_RESPONSE" | jq -r ."[$num].tag_name")
+      local ASSETS_LENGTH=$(echo "$CURL_RESPONSE" | jq -r ."[$num].assets | length")
+      for ((i = 0 ; i < $ASSETS_LENGTH ; i++)); do
+         local ASSET_NAME=$(echo "$CURL_RESPONSE" | jq -r ."[$num].assets[$i].name")
+         if echo "$ASSET_NAME" | rg -q ".tar.gz" ; then
+            TARBALL_URL=$(echo "$CURL_RESPONSE" | jq -r ."[$num].assets[$i].browser_download_url")
+         fi
+         if echo "$ASSET_NAME" | rg -q ".sha512sum" ; then
+            SHA512_URL=$(echo "$CURL_RESPONSE" | jq -r ."[$num].assets[$i].browser_download_url")
+         fi
+      done
+
+      if [ -z "$TARBALL_URL" ] || [ -z "$SHA512_URL" ]; then
+         echo "Could not find URLs"
+         continue
+      else
+         echo "$VERSION found"
+         TARBALLS["$VERSION"]="$TARBALL_URL"
+         SHA512["$VERSION"]="$SHA512_URL"
+         local TARBALL_NAME=$(basename "$TARBALL_URL")
+         local DIR_NAME=${TARBALL_NAME%.tar.gz}
+         if [ -d "$STEAM_PATH/$DIR_NAME" ]; then
+            INSTALLED["$VERSION"]=1
+         else
+            INSTALLED["$VERSION"]=0
+         fi
       fi
    done
-
-   if [ -z "$TARBALL_URL" ] || [ -z "$SHA512_URL" ]; then
-      echo "Could not find URLs"
-      exit 1
-   fi
-
-   TARBALL_NAME=$(basename "$TARBALL_URL")
-   SHA512_NAME=$(basename "$SHA512_URL")
-   DIR_NAME=${TARBALL_NAME%.tar.gz}
 }
+
+   #local TARBALL_NAME=$(basename "$TARBALL_URL")
+   #local SHA512_NAME=$(basename "$SHA512_URL")
+   #local DIR_NAME=${TARBALL_NAME%.tar.gz}
 
 function download(){
    echo "Downloading tarball: $TARBALL_NAME"
@@ -102,7 +114,7 @@ function check_if_installed(){
    fi
 }
 
-get_urls "$LATEST_URL"
+get_urls "$RELEASES_URL"
 check_if_installed
 download
 
