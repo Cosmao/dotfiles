a75230748cdf9b586e9e7cdf382ac02afde73859
Emil K <29043737+Cosmao@users.noreply.github.com>
2026-03-31 15:42:10 +0200

    Fixes for new system

--- .config/nvim/lua/kickstart/plugins/lsp.lua
+++ .config/nvim/lua/kickstart/plugins/lsp.lua
@@ -166,8 +166,8 @@ return {
       local servers = {
         clangd = {},
         -- gopls = {},
-        pyright = {},
-        rust_analyzer = {},
+        pylsp = {},
+        -- rust_analyzer = {},
         -- ts_ls = {},
 
         lua_ls = {
